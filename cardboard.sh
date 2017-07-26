b645479f2bbcbac4c5c2b782b43e6e680b7922a3
Mahmud Ridwan <m@hjr265.me>
2017-07-26 22:54:37 +0600

    Add build and push script

---
+++ cardboard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CMD=$1
+BOX=$2
+
+case $CMD in
+	build)
+		docker build -f $BOX/Dockerfile -t cardboard/$BOX:`cat $BOX/VERSION` .
+		;;
+
+	push)
+		docker push cardboard/$BOX:`cat $BOX/VERSION`
+		;;
+
+esac
